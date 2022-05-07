e14d9f083ec85764fce0e2b516625d6b49452ed2
amartya49 <105108743+amartya49@users.noreply.github.com>
2022-05-07 16:53:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function onClickMenu(){
+    document.getElementById("hamburger").classList.toggle("icon")
+    document.getElementById("menuItems").classList.toggle("change")
+}
+
+
+// smooth scrooling
+
+
